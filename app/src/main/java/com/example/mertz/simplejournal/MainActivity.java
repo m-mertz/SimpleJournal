91137fcc00208aa246054ab4c405476985ac5cb0
Matthias Mertz <mertz.matthias@gmail.com>
2017-08-11 15:06:10 +0200

    load values in async task

--- app/src/main/java/com/example/mertz/simplejournal/MainActivity.java
+++ app/src/main/java/com/example/mertz/simplejournal/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mertz.simplejournal;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -14,9 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         m_storageService = new JournalStorageService(this);
 
-        String testValue = m_storageService.GetGratefulnessEntry("2017-08-11", 0);
-        EditText editText = (EditText)findViewById(R.id.Gratefulness0Input);
-        editText.setText(testValue);
+        new LoadValuesTask().execute();
     }
 
     @Override
@@ -26,4 +25,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private JournalStorageService m_storageService;
+
+    // TODO two tasks for read / write, or an intent service? intent service returns values in some
+    // async way, but not sure how much boilerplate required to await results and write to UI.
+    // if service interface is just about data and synchronous, and we have two background tasks here
+    // to call it and link it to UI, that's not too bad.
+    private class LoadValuesTask extends AsyncTask<Object, Integer, String> {
+
+        @Override
+        protected String doInBackground(Object[] params) {
+            String gratefulness0 = m_storageService.GetGratefulnessEntry("2017-08-11", 0);
+            return gratefulness0;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            EditText editText = (EditText)findViewById(R.id.Gratefulness0Input);
+            editText.setText(s);
+        }
+    }
 }
